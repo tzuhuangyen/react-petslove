52b6b13b77f11fdb879c04ca52774feb451227c7
tzuhuangyen <912yan@gmail.com>
2024-01-19 21:16:50 +0800

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-function App() {
-  const [count, setCount] = useState(0);
+import "./App.css";
+import "./assets/all.scss";
+import Home from "./assets/pages/Home";
+import Header from "./assets/pages/Header";
 
+function App() {
   return (
     <>
-      <button type="button" class=" testBtn">
+      <Header />
+      <button type="button" className=" testBtn">
         test
       </button>
-      <button type="button" class="btn btn-secondary">
-        Secondary
-      </button>
-      <button type="button" class="btn btn-success">
-        Success
-      </button>
-      <button type="button" class="btn btn-danger">
-        Danger
-      </button>
-      <button type="button" class="btn btn-warning">
-        Warning
-      </button>
-      <button type="button" class="btn btn-info">
-        Info
-      </button>
-      <button type="button" class="btn btn-light">
-        Light
-      </button>
-      <button type="button" class="btn btn-dark">
-        Dark
-      </button>
-
-      <button type="button" class="btn btn-link">
-        Link
-      </button>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/react-petslove" element={<Home />} />
+        <Route path="/shop" />
+        {/* <Route path="/user" element={<Login />} />
+        <Route path="/favorite" element={<Favorite />} /> */}
+      </Routes>
     </>
   );
 }
